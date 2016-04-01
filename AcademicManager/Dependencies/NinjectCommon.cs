44399d6c858e6f92d300d953abc867c7a829c780
cornel1923 <cornel1923@yahoo.com>
2016-04-01 11:55:11 +0300

    Add Dependencies project
    * Through this project we can have all dependencies in one place

---
+++ AcademicManager/Dependencies/NinjectCommon.cs
@@ -0,0 +1,18 @@
+﻿using Database.Implementations;
+using Database.Interfaces;
+using Ninject.Modules;
+using ProcessManagement.Implementations;
+using ProcessManagement.Interfaces;
+
+namespace Dependencies
+{
+    public class NinjectCommon : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IStudent>().To<Student>();
+            Bind<IDatabase>().To<FileSystemDatabase>();
+            Bind<IDatabaseManager<IDatabase>>().To<FileSystemDatabaseManager>();
+        }
+    }
+}
