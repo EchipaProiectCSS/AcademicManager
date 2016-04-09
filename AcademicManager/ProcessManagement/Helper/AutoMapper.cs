c3796af6c86688ec0f89b5514b8e6c9270836de4
cornel1923 <cornel1923@yahoo.com>
2016-04-09 22:56:43 +0300

    AutoMapper
    Dynamic mapper for [column - entity property]
    - the name of the property must be the same with the name of the table
    column

---
+++ AcademicManager/ProcessManagement/Helper/AutoMapper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Database.Implementations.Internal.Domain;
+
+namespace ProcessManagement.Helper
+{
+    public class AutoMapper
+    {
+        private readonly object dataDo;
+        private readonly Dictionary<string, string> row;
+
+        public AutoMapper(Object dataDo, Dictionary<string,string> row)
+        {
+            this.dataDo = dataDo;
+            this.row = row;
+        }
+
+        public void Start()
+        {
+            foreach (var property in dataDo.GetType().GetProperties())
+            {
+                SetRowValueToObject(property.Name);
+            }
+        }
+
+        private void SetRowValueToObject(string columnName)
+        {
+            //TODO:test these scenario's
+
+            var type = dataDo.GetType().GetProperty(columnName).PropertyType;
+
+            CheckIfProertyMatchNamingConvention(columnName);
+
+            SetColumnValueToObjectProperty(columnName, type);
+        }
+
+        private void SetColumnValueToObjectProperty(string columnName, Type type)
+        {
+            var rowValue = row.Single(k => k.Key.Equals(columnName)).Value;
+
+            dataDo.GetType().GetProperty(columnName).SetValue(dataDo, Cast(rowValue, type));
+        }
+
+        private void CheckIfProertyMatchNamingConvention(string columnName)
+        {
+            if (row.Count(k => k.Key.Equals(columnName)) == 0)
+            {
+                throw new Exception("The object " + dataDo.GetType().Name + " should respect the name convention for " +
+                                    columnName);
+            }
+        }
+
+        public static dynamic Cast(dynamic obj, Type castTo)
+        {
+            return Convert.ChangeType(obj, castTo);
+        }
+    }
+}
