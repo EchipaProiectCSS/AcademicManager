bba50fd6a34c4b1c18811528de077851f39a5039
cornel1923 <cornel1923@yahoo.com>
2016-04-09 23:02:19 +0300

    Student Repository
    Add new class in order to handle data requests for students

---
+++ AcademicManager/ProcessManagement/Implementations/StudentRepository.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Database.Interfaces;
+using Database.Interfaces.Internal;
+using ProcessManagement.DOs;
+using ProcessManagement.Helper;
+using ProcessManagement.Interfaces;
+
+namespace ProcessManagement.Implementations
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private readonly IDatabase database;
+
+        public StudentRepository(IDatabaseModel model)
+        {
+            database = model.GetInstance();
+        }
+
+        public List<StudentDO> GetAll()
+        {
+            var query = database.Query(Queries.GetAllStudents);
+
+            var students = GetStudentsFromQueryResult(query);
+
+            return students;
+        }
+
+        private List<StudentDO> GetStudentsFromQueryResult(IQueryResult query)
+        {
+            List<StudentDO> students = new List<StudentDO>();
+
+            foreach (var row in query.Result.Rows)
+            {
+                StudentDO studentDo = new StudentDO();
+
+                new AutoMapper(studentDo, row.Values).Start();
+
+                students.Add(studentDo);
+            }
+
+            return students;
+        }
+    }
+}
---
+++ AcademicManager/ProcessManagement/Interfaces/IStudentRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using ProcessManagement.DOs;
+
+namespace ProcessManagement.Interfaces
+{
+    public interface IStudentRepository
+    {
+        List<StudentDO> GetAll();
+    }
+}
