afb795db5d13fbe424ee7e9dec61c9d01d9f9289
cornel1923 <cornel1923@yahoo.com>
2016-04-09 22:58:39 +0300

    Queries
    Add a Static Class in order to hold the queries

---
+++ AcademicManager/ProcessManagement/Helper/Queries.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ProcessManagement.Helper
+{
+    public class Queries
+    {
+        public static string GetAllStudents = "select * from students;";
+    }
+}
