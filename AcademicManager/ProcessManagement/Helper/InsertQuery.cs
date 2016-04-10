c68d202520d912bb63f8e520374151f02b6691d8
cornel1923 <cornel1923@yahoo.com>
2016-04-10 16:01:52 +0300

    Generic Queries
    Add a way to create generic queries for update and insert operations
    The query is created based on all properties of the received object
    The object sent as parameter must be an entity that reflect table in
    database

---
+++ AcademicManager/ProcessManagement/Helper/InsertQuery.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace ProcessManagement.Helper
+{
+    public class InsertQuery
+    {
+        public static string Create(string tableName, object objectData)
+        {
+            var query = $"insert into {tableName} ";
+            var last = objectData.GetType().GetProperties().Last();
+            var columns = "(";
+            var values = "(";
+
+            foreach (var property in objectData.GetType().GetProperties())
+            {
+                var concate = property.Equals(last) ? ")" : ", ";
+
+                columns += $"{property.Name}{concate}";
+                values += $"'{property.GetValue(objectData)}'{concate}";
+            }
+
+            query += $"{columns} values {values};";
+
+            return query;
+        }
+    }
+}
---
+++ AcademicManager/ProcessManagement/Helper/UpdateQuery.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace ProcessManagement.Helper
+{
+    public static class UpdateQuery
+    {
+        public static string Create(string tableName, object objectData)
+        {
+            var query = $"update {tableName} set ";
+
+            var last = objectData.GetType().GetProperties().Last();
+
+            var Id = "0";
+
+            foreach (var property in objectData.GetType().GetProperties())
+            {
+                if (property.Name.ToLower() == "id")
+                {
+                    Id = property.GetValue(objectData).ToString();
+                    continue;
+                }
+
+                query += $"{property.Name} = '{property.GetValue(objectData)}'";
+
+                query += property.Equals(last) ? " " : ", ";
+            }
+
+            query += $"where Id = '{Id}';";
+
+            return query;
+        }
+    }
+}
