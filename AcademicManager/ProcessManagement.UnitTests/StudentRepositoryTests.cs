65af66a34290a0d1b87c0ddcee2c2644e95315f0
cornel1923 <cornel1923@yahoo.com>
2016-04-10 16:02:43 +0300

    Add tests for StudentStatusRepository

---
+++ AcademicManager/ProcessManagement.UnitTests/StudentRepositoryTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using ProcessManagement.DOs;
+using ProcessManagement.Implementations;
+using ProcessManagement.Interfaces;
+
+namespace ProcessManagement.UnitTests
+{
+    [TestFixture]
+    public class StudentRepositoryTests
+    {
+        private IDatabaseModel databaseModel;
+        private StudentRepository student;
+
+        [SetUp]
+        public void Init()
+        {
+            databaseModel = new DatabaseModel();
+            student = new StudentRepository(databaseModel);
+        }
+
+        [Test]
+        public void ChecIfStudentIsInDatabaseTest()
+        {
+            var result = student.Get(1);
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void CheckIfStudentIsNotInDatabaseTest()
+        {
+            var result = student.Get(100);
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void UpdateStudentDataTest()
+        {  
+            //TODO: not complete
+            StudentDo studentData = new StudentDo()
+            {
+                Id = 0,
+                Age = "22",
+                FirstName = "Cornel"
+            };
+
+            student.Update(studentData);
+        }
+
+        [Test]
+        public void InsertStudentDataTest()
+        {
+            //TODO: not complete
+            StudentDo studentData = new StudentDo()
+            {
+                Id = 3,
+                Age = "25",
+                FirstName = "Cornel"
+            };
+
+            student.Insert(studentData);
+        }
+    }
+}
---
+++ AcademicManager/ProcessManagement.UnitTests/StudentStatusRepositoryTest.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using ProcessManagement.DOs;
+using ProcessManagement.Implementations;
+using ProcessManagement.Interfaces;
+
+namespace ProcessManagement.UnitTests
+{
+    [TestFixture]
+    public class StudentStatusRepositoryTest
+    {
+        private IDatabaseModel databaseModel;
+        private StudentStatusRepository studentStatus;
+
+        [SetUp]
+        public void Init()
+        {
+            databaseModel = new DatabaseModel();
+            studentStatus = new StudentStatusRepository(databaseModel);
+        }
+
+        [Test]
+        public void GetAllStatusesTest()
+        {
+            var result = studentStatus.GetAll();
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void GetAllStatusesByStudentIdTest()
+        {
+            var result = studentStatus.GetStudentStatuses(1);
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void GetStatusbyStatusIdTest()
+        {
+            var result = studentStatus.Get(1);
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void UpdateStudentDataTest()
+        {
+            //TODO: not complete
+            StudentStatusDo studentData = new StudentStatusDo()
+            {
+                Id = 0,
+                Credits = 40,
+                StudentId = 1,
+                ECTS = 8
+            };
+
+            studentStatus.Update(studentData);
+        }
+
+        [Test]
+        public void InsertStudentDataTest()
+        {
+            //TODO: not complete
+            StudentStatusDo studentData = new StudentStatusDo()
+            {
+                Id = 2,
+                Credits = 33,
+                StudentId = 1,
+                ECTS = 8,
+                Year = 2016
+            };
+
+            studentStatus.Insert(studentData);
+        }
+
+    }
+}
